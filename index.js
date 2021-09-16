f3b7952a0211b030edb3fd8b5161ac16986146c4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-16 13:19:35 -0500

    commit tree 4075817028c215074a7efd4e0510692c20fcfa95

---
+++ index.js
@@ -0,0 +1,36 @@
+//* fuction declaration
+// function fooOne() {
+//   return "bar";
+// }
+//*funtion expression
+// let fooTwo = function() {
+//     return "bar"
+// }
+//* IIFE - Immediately Invoked Function Declration
+// let foo2 = (function() {
+//     return "bar"
+// })()
+//*arrow function
+// let fooThree = () => "bar"
+//=> function declaration
+// console.log(fooTwo)
+//=> 
+// console.log(fooTwo())
+//=>bar
+// console.log(foo2)
+//=> bar
+// console.log(foo2())
+//=> error, foo2 is not a function
+
+
+let divide = () => 2000 / 100
+// console.log(divide(2000,100))
+
+
+let square = num => num ** 2
+
+
+let add = (num1, num2) => {
+    return num1 + num2
+}
+
